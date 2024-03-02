e0bcb456f148f89ea81023e54d99f42937a03fa0
JuliaTarasenko00 <118853187+JuliaTarasenko00@users.noreply.github.com>
2024-03-02 16:07:46 +0000

    Deploying to gh-pages from @ JuliaTarasenko00/kindzmarauli@06e529f5859c7f46f39d05b467399efdf353e75f 🚀

---
+++ assets/Search.styled-OYaMw3dH.js
@@ -0,0 +1,64 @@
+import{g as o}from"./index-DfdUAF4G.js";const r=o.button`
+  cursor: pointer;
+
+  background-color: transparent;
+  border: none;
+  color: var(--main-color);
+  font-size: 24px;
+  transition: color var(--transition);
+
+  &:hover,
+  :focus {
+    color: var(--active-color);
+  }
+`,n=o.div`
+  display: block;
+  margin-bottom: 20px;
+
+  @media screen and (min-width: 768px) {
+    margin-bottom: 0;
+  }
+`;o.form``;const a=o.div`
+  position: relative;
+`,i=o.p`
+  position: absolute;
+  bottom: -25px;
+  left: 0;
+  color: #ff000091;
+  width: max-content;
+`,e=o.input`
+  padding: 10px;
+  background: transparent;
+  border-radius: 10px;
+  border: 2px solid #ffffff63;
+  height: 44px;
+  color: var(--main-color);
+  outline-color: transparent;
+`,s=o.div`
+  position: absolute;
+  right: 15px;
+  top: 50%;
+  transform: translateY(-50%);
+
+  display: flex;
+  align-items: center;
+  gap: 8px;
+`,p=o.button`
+  border: none;
+  background-color: transparent;
+  color: grey;
+
+  & svg {
+    width: 15px;
+    height: 15px;
+  }
+
+  transition: color var(--transition);
+
+  &:hover,
+  :focus {
+    color: var(--active-color);
+  }
+`,c=o.section`
+  padding-bottom: 40px;
+`;export{r as B,p as D,i as E,c as F,e as I,a as W,n as a,s as b};
